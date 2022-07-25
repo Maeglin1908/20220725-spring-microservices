729b49c60b0d7c7764c2458d7b3e9445a217c0f6
Nilgam <nilgam@proton.me>
2022-07-25 13:59:33 +0200

    spring boot

---
+++ springboot/src/main/java/fr/dawan/springboot/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package fr.dawan.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+}
